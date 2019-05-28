bce4e5a6f261861c4bd841e1321135b64fdf4e52
Lewis Crichton <lewistehminerz@outlook.com>
2019-05-28 10:58:15 +0100

    remove vue

--- src/frontend/controller.ts
+++ src/frontend/controller.ts
@@ -1,17 +1,12 @@
 import { Controller, ClassMiddleware, Get } from '@overnightjs/core';
-import * as ExpressVue from 'express-vue';
 import * as path from 'path';
 
 import Logger from '@ayana/logger';
 
 import { Response } from 'express';
 
-const vue = ExpressVue.init({
-    rootPath: path.join(__dirname, '../../vue') // this is the 'vue' directory in the root of the project
-} as any) as any; // poor typings unfortunately
 
 @Controller('/')
-@ClassMiddleware([vue])
 export class FrontendController {
     private logger: Logger;
 
@@ -20,13 +15,6 @@ export class FrontendController {
     }
 
     @Get('/')
-    public rootPageHandler(req: Request, res: ExpressVueResponse) {
-        res.renderVue('home.vue');
+    public rootPageHandler(req: Request, res: Response) {
     }
-}
-
-// custom typedefs due to Response not having .renderVue
-
-interface ExpressVueResponse extends Response {
-    renderVue(componentPath: string, data?: any, vueOptions?: any): void;
 }
--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,6 @@ const cascadeLogger = Logger.get('bootstrap');
 if (process.env.NODE_ENV !== 'production') {
     Logger.getDefaultTransport().setLevel(LogLevel.DEBUG); // globally sets the min log level to debug, in production this doesn't happen
     cascadeLogger.warn('Cascade is running in development mode. Set NODE_ENV to "production" to disable debug output.');
-
-    // this is a hack
-    global.console.info = (str: string) => { // express-vue uses this (annoyingly)
-        cascadeLogger.debug(str);
-    };
-} else {
-    global.console.info = () => {}; // mutes express-vue
 }
 
 cascadeLogger.info('Cascade is starting now.');
