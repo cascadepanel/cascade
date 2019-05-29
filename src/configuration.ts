00e3ab73b54462075a28b427338f23597935c775
Lewis Crichton <lewistehminerz@outlook.com>
2019-05-29 14:15:02 +0100

    start configuration

---
+++ src/configuration.ts
@@ -0,0 +1,5 @@
+export default interface Configuration {
+    debug: boolean;
+    port: number;
+    'admin-password-override': string;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import Logger, { LogLevel } from '@ayana/logger';
 import { CascadeServer } from './server';
+import TOML from 'toml';
+import * as path from 'path';
+import Configuration from './configuration';
 
 const cascadeLogger = Logger.get('bootstrap');
+const config = TOML.parse(path.join(__dirname, '../config.toml')) as Configuration;
 
-if (process.env.NODE_ENV !== 'production') {
+if (config.debug) {
     Logger.getDefaultTransport().setLevel(LogLevel.DEBUG); // globally sets the min log level to debug, in production this doesn't happen
-    cascadeLogger.warn('Cascade is running in development mode. Set NODE_ENV to "production" to disable debug output.');
+    cascadeLogger.warn('Cascade is running in development mode. Set the "debug" property in the config file to false to disable debug output.');
 }
 
 cascadeLogger.info('Cascade is starting now.');
 
 const server = new CascadeServer();
-server.start(8000, () => {
-    cascadeLogger.info('Cascade is running on port 8000.');
+server.start(config.port, () => {
+    cascadeLogger.info(`Running on port ${config.port}.`);
 });
